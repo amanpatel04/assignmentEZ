e1398ebbaa158c99be09bbc7790f4f2c7cbf4938
aman219 <65351629+aman219@users.noreply.github.com>
2025-06-26 00:17:41 +0530

    Added : Postman exprot json

--- fileshare/files/urls.py
+++ fileshare/files/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
   path('', views.get_file, name='get_file'),
   path('upload/', views.upload_file, name='upload_file'),
+  path('download/', views.download_file, name='download_file'),
+  path('download/secure/', views.secure_download, name='secure_download'),
 ]
--- fileshare/files/views.py
+++ fileshare/files/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from .models import File, File_User
 from django.views.decorators.csrf import csrf_exempt
 from user.views import is_logged_in
+from django.shortcuts import redirect
+from django.conf import settings
+
+import jwt
+import os
 # Create your views here.
 def get_file(request):
   files = File.objects.all()
@@ -24,3 +29,32 @@ def upload_file(request):
       File_User.objects.create(user=user, file=file)
       return JsonResponse({'success':True}, safe=False)
   return JsonResponse(status=400, data={'success':False}, safe=False)
+
+
+def download_file(request):
+  if (request.COOKIES.get('token') == None):
+    return JsonResponse(status=400, data={'success':False}, safe=False)
+  qurey = request.GET.get('file_id')
+  file = File.objects.filter(id=qurey).first()
+  user = is_logged_in(request.COOKIES['token'])
+  payload = {
+    'download_link' : file.file.url,
+    'message': 'success'
+  }
+  token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+  url = request.build_absolute_uri('/api/v1/file/download/secure/?token=' + token)
+  return redirect(url)
+
+
+def secure_download(request):
+  # if (request.GET.get('token') == None):
+  #   return JsonResponse(status=400, data={'success':False}, safe=False)
+  try:
+    payload = jwt.decode(request.GET.get('token'), settings.SECRET_KEY, algorithms=['HS256'])
+    file_path = os.path.join(settings.BASE_DIR, payload['download_link'][1:])
+    if not os.path.exists(file_path):
+      return JsonResponse(status=400, data={'message': 'File not found'}, safe=False)
+    return FileResponse(open(file_path, 'rb'))
+  except Exception as e:
+    print("JWT decode error:", e)
+    return JsonResponse(status=400, data={'success':False}, safe=False)
